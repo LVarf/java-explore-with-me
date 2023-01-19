613decd15f9fc85d5c695c279877cb78bc469e68
Leonid Varfolomeev <lvarfolomeev@at-consulting.ru>
2023-01-19 10:28:50 +0300

    EWM-fr: feat tests

---
+++ ewm-core/src/test/java/ru/practicum/ewmcore/service/reportService/ReportsServiceImplTest.java
@@ -0,0 +1,42 @@
+package ru.practicum.ewmcore.service.reportService;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportsServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void readAllReports() {
+    }
+
+    @Test
+    void readReport() {
+    }
+
+    @Test
+    void updateReport() {
+    }
+
+    @Test
+    void createCommentReportPublic() {
+    }
+
+    @Test
+    void createEventReportPublic() {
+    }
+
+    @Test
+    void createUserReportPublic() {
+    }
+}

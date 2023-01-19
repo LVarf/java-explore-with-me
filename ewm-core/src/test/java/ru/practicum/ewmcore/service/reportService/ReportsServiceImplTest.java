94ebb48bf86201ebbe6b884889e82245467eb8d8
Leonid Varfolomeev <lvarfolomeev@at-consulting.ru>
2023-01-19 16:54:11 +0300

    EWM-fr: feat tests

--- ewm-core/src/test/java/ru/practicum/ewmcore/service/reportService/ReportsServiceImplTest.java
+++ ewm-core/src/test/java/ru/practicum/ewmcore/service/reportService/ReportsServiceImplTest.java
@@ -1,15 +1,38 @@
 package ru.practicum.ewmcore.service.reportService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.practicum.ewmcore.model.reports.Report;
+import ru.practicum.ewmcore.model.reports.ReportEntityEnum;
+import ru.practicum.ewmcore.repository.ReportRepository;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
 
+
+@ExtendWith(MockitoExtension.class)
 class ReportsServiceImplTest {
+
+    @Mock
+    private ReportRepository repository;
+    private Report report;
 
     @BeforeEach
     void setUp() {
+        report = new Report().setId(1L)
+                .setEntity(ReportEntityEnum.COMMENT)
+                .setActual(true);
     }
 
     @AfterEach
@@ -22,6 +45,14 @@ class ReportsServiceImplTest {
 
     @Test
     void readReport() {
+        Mockito
+                .when(repository.findById(anyLong()))
+                .thenReturn(Optional.ofNullable(report));
+        Report newReport = repository.findById(1L).orElse(null);
+        assertNotNull(newReport);
+        assertEquals(newReport.getId(), report.getId());
+        assertEquals(newReport.getActual(), report.getActual());
+        assertEquals(newReport.getEntity(), report.getEntity());
     }
 
     @Test
